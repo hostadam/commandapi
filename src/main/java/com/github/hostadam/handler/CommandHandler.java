a6019eca35b350f618552a6214bf99feac6b3e88
hostadam <hostadam.minecraft@gmail.com>
2023-08-14 18:18:37 +0200

    forgot to add it to map lol.

--- src/main/java/com/github/hostadam/handler/CommandHandler.java
+++ src/main/java/com/github/hostadam/handler/CommandHandler.java
@@ -7,12 +7,17 @@ import com.github.hostadam.parameter.ParameterConverter;
 import com.github.hostadam.parameter.impl.BooleanConverter;
 import com.github.hostadam.parameter.impl.IntConverter;
 import com.github.hostadam.parameter.impl.PlayerConverter;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.CommandMap;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
 
 public class CommandHandler {
 
+    private CommandMap map;
     private final Map<Class<?>, ParameterConverter<?>> parameters;
     private final Map<String, BukkitCommand> commands;
 
@@ -20,12 +25,22 @@ public class CommandHandler {
         this.parameters = new HashMap<>();
         this.commands = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
+        try {
+            final Server server = Bukkit.getServer();
+            Field field = server.getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+            this.map = (CommandMap) field.get(server);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         this.addConverter(Boolean.class, new BooleanConverter());
         this.addConverter(IntConverter.class, new IntConverter());
         this.addConverter(PlayerConverter.class, new PlayerConverter());
     }
 
     public void register(Object object) {
+        if(this.map == null) return;
         for(Method method : object.getClass().getMethods()) {
             if(!method.isAnnotationPresent(Command.class)) {
                 continue;
@@ -46,6 +61,7 @@ public class CommandHandler {
                 }
 
                 BukkitCommand bukkitCommand = new BukkitCommand(this, impl, name, command.description(), command.usage(), aliases);
+                this.map.register(name, bukkitCommand);
                 aliases.forEach(alias -> this.commands.put(alias, bukkitCommand));
             }
         }
